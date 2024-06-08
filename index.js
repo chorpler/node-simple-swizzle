7950c2dbf1ccd0c702ffa22ea0fe437f9fc7a838
David Sargeant <chorpler@gmail.com>
2024-06-07 19:29:26 -0600

    Update for combining

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 var isArrayish = require('is-arrayish');
 
+var isArrayish = function(obj) {
+	if (!obj || typeof obj === 'string') {
+		return false;
+	}
+
+	return obj instanceof Array || Array.isArray(obj) ||
+		(obj.length >= 0 && (obj.splice instanceof Function ||
+			(Object.getOwnPropertyDescriptor(obj, (obj.length - 1)) && obj.constructor.name !== 'String')));
+};
+
 var concat = Array.prototype.concat;
 var slice = Array.prototype.slice;
 
